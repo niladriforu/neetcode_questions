5231ae076577b2fc5cdd3b8496882334939acc46
niladri <niladriforu@gmail.com>
2025-03-03 23:13:12 -0800

    sliding_window bruteforce method for finding duplicates in N window

--- neetcode_questions/sliding_window.py
+++ neetcode_questions/sliding_window.py
@@ -1,21 +1,30 @@
 def closeDupplicatesBruteForce(mylist, size):
-    for left in range(len(mylist)-1):
+    '''
+    This function takes a list and a window size and checks if there are any duplicates in the window
+    This can be also adjacent two numbers in the list that are equal #for right in range(left+1, min(len(mylist),left+size))
+    this can be also two alternate numbers in the list that are equal #for right in range(left+2, min(len(mylist),left+size+1))
+    :param mylist:
+    :param size:
+    :return:
+    '''
+    for left in range(len(mylist)):
             print(f'left is {left}')
-            for right in range(left+1, left+size) :
+            # for right in range(left+2, min(len(mylist),left+size+1)) :
+            for right in range(left+1, min(len(mylist),left+size)) :
                 # It actually means that the window is from left to left+size
                 # and we are taking the first element of the window and checking
                 # if the other elements in the window are same
-                if len(mylist) >=  left+size:
-                    print(f'range is  : {left} "::"  {left+size}')
-                    print('00000000000')
-                    if mylist[left] == mylist[right]:
-                        print(f'found a duplicate at {left} and {right}')
-                        return True
+                # if len(mylist) >=  left+size:
+                print(f'range is  : {left} "::"  {left+size}')
+                print('00000000000')
+                if mylist[left] == mylist[right]:
+                    print(f'found a duplicate at {left} and {right}')
+                    return True
     return False
 
 
 if __name__ == '__main__':
-    mylist = [ 1,2,3,2,23,4]
-    window_size = 2
+    mylist = [ 1,2,2,5,3,5]
+    window_size = 3
     print(closeDupplicatesBruteForce(mylist,window_size))
     # closeDupplicatesBruteForce(mylist,window_size)
